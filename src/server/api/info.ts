27cb594c0096c2ea120ba996470a87ab77768cf6
meitrix8208 <maurrod2001@outlook.com>
2025-02-07 11:32:44 -0500

    feat: Improve geo information handling in info API by using default base64 data and refining response structure

--- src/server/api/info.ts
+++ src/server/api/info.ts
@@ -1,36 +1,38 @@
 export default defineEventHandler(async (event) => {
+  const base64default = "eyJjaXR5IjoiQmFycmFucXVpbGxhIiwiY291bnRyeSI6eyJjb2RlIjoiQ08iLCJuYW1lIjoiQ29sb21iaWEifSwicG9zdGFsX2NvZGUiOiIwODAwMDEiLCJzdWJkaXZpc2lvbiI6eyJjb2RlIjoiQVRMIiwibmFtZSI6IkF0bMOhbnRpY28ifSwidGltZXpvbmUiOiJBbWVyaWNhL0JvZ290YSIsImxhdGl0dWRlIjoxMC45NzExLCJsb25naXR1ZGUiOi03NC43ODM3fQ=="
+  const headers = getRequestHeaders(event);
   const ip = getRequestIP(event, {
     xForwardedFor: true,
   });
+  const geoDummy = JSON.parse(Buffer.from(base64default, "base64").toString("utf-8"));
   //* If the IP is localhost, return a hardcoded value
-  const headers = getRequestHeaders(event);
   if (ip === "127.0.0.1") {
     return {
       city: "Localhost",
       ip,
-      headers
+      geoDummy,
     };
   }
-
-  const url = `https://ip.guide/${ip}`;
-  // headers["x-nf-geo"] 
-  headers["x-nf-geo"] = headers["x-nf-geo"] || "eyJkYXRhIjp7ImNpdHkiOiJNYXJrZW5lIn19";
-  const geo = JSON.parse(Buffer.from(headers["x-nf-geo"], "base64").toString("utf-8"));
+  interface Geo {
+    city: string;
+    country: Country
+  }
+  interface Country {
+    code: string;
+    name: string;
+  }
 
   try {
-    const { location: { city } } = await $fetch<{ location: { city: string } }>(url, {
-      responseType: "json",
-    });
+    headers["x-nf-geo"] = headers["x-nf-geo"] || base64default;
+    const geo: Geo = JSON.parse(Buffer.from(headers["x-nf-geo"], "base64").toString("utf-8"));
     return {
-      city,
+      city: `${geo.country.name}/${geo.city}`,
       ip,
-      headers, 
-      geo
     };
   } catch (error) {
     throw createError({
       statusCode: 500,
-      message: "Failed to fetch data",
+      message: "Failed to get geo data",
       data: error,
     });
   }
